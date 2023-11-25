45a60ddf57b5372b8849662ddeb46eeb849860a3
zhangyue44 <zy1617768774@163.com>
2023-11-25 16:48:33 +0800

    feat: 引入koa2-cors配置跨域

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,18 @@
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
+const Koa = require("koa");
+const cors = require("koa2-cors");
+const bodyParser = require("koa-bodyparser");
 
-const useRoutes = require('../router') 
+const useRoutes = require("../router");
 
 const app = new Koa();
 
-const errorHandle = require('./err-handle')
+const errorHandle = require("./err-handle");
 
+app.use(cors());
 app.use(bodyParser());
 
-useRoutes(app); 
+useRoutes(app);
 
-app.on('error', errorHandle);
+app.on("error", errorHandle);
 
 module.exports = app;
